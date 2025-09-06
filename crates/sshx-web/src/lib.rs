8caf9ae39a9b975fca36aac3da6692a74d65ea93
Sterne Lee <sternelee@gmail.com>
2025-09-06 13:44:59 +0800

    feat: 尝试 wasm 版本的 sshx-web

--- crates/sshx-web/src/lib.rs
+++ crates/sshx-web/src/lib.rs
@@ -1,121 +1,262 @@
+use std::{
+    collections::BTreeSet,
+    sync::{Arc, Mutex},
+};
+
 use anyhow::Result;
+use data_encoding::BASE32_NOPAD;
 use futures_lite::StreamExt;
-use iroh::net::{Endpoint, NodeAddr};
 use iroh::protocol::Router;
-use iroh::SecretKey;
+use iroh::{Endpoint, NodeAddr, SecretKey, Watcher};
 use iroh_gossip::{
-    net::{Event, Gossip, GossipEvent},
+    api::{Event, GossipReceiver, GossipSender},
+    net::{Gossip, GOSSIP_ALPN},
     proto::TopicId,
 };
-use prost::Message;
-use rand::rngs::OsRng;
+use n0_future::{time::Duration, StreamExt as _};
+use rand::{rngs::OsRng, RngCore};
 use serde::{Deserialize, Serialize};
-use sshx_core::proto::{ClientUpdate, ServerUpdate};
-use std::str::FromStr;
-use wasm_bindgen::prelude::*;
-use web_sys::console;
+use sshx_core::{rand_alphanumeric, Sid};
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber_wasm::MakeConsoleWriter;
+use wasm_bindgen::{prelude::wasm_bindgen, JsError, JsValue};
+use wasm_streams::ReadableStream;
 
-#[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
+#[wasm_bindgen(start)]
+fn start() {
+    console_error_panic_hook::set_once();
+
+    tracing_subscriber::fmt()
+        .with_max_level(LevelFilter::DEBUG)
+        .with_writer(
+            // To avoid trace events in the browser from showing their JS backtrace
+            MakeConsoleWriter::default().map_trace_level_to(tracing::Level::DEBUG),
+        )
+        // If we don't do this in the browser, we get a runtime error.
+        .without_time()
+        .with_ansi(false)
+        .init();
+
+    tracing::info!("SSHX WASM module initialized");
 }
 
-macro_rules! console_log {
-    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
-}
-
+/// A ticket that contains the necessary information to join a session.
 #[derive(Debug, Serialize, Deserialize)]
 struct Ticket {
+    /// The gossip topic to join.
     topic: TopicId,
+    /// The node addresses of the host.
     nodes: Vec<NodeAddr>,
+    /// The encryption key for the session.
     key: String,
 }
 
 impl Ticket {
+    /// Deserialize from a slice of bytes to a Ticket.
     fn from_bytes(bytes: &[u8]) -> Result<Self> {
         serde_json::from_slice(bytes).map_err(Into::into)
     }
+
+    /// Serialize from a `Ticket` to a `Vec` of bytes.
+    pub fn to_bytes(&self) -> Vec<u8> {
+        serde_json::to_vec(self).expect("serde_json::to_vec is infallible")
+    }
 }
 
-impl FromStr for Ticket {
+impl std::fmt::Display for Ticket {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let mut text = BASE32_NOPAD.encode(&self.to_bytes()[..]);
+        text.make_ascii_lowercase();
+        write!(f, "{}", text)
+    }
+}
+
+impl std::str::FromStr for Ticket {
     type Err = anyhow::Error;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let s = s.split(',').next().unwrap_or(s); // Ignore write password for now
-        let bytes = data_encoding::BASE32_NOPAD.decode(s.to_ascii_uppercase().as_bytes())?;
+        let bytes = BASE32_NOPAD.decode(s.to_ascii_uppercase().as_bytes())?;
         Self::from_bytes(&bytes)
     }
 }
 
+/// Node for SSH sessions over iroh-gossip
 #[wasm_bindgen]
-pub struct WebClient {
-    sender: iroh_gossip::net::GossipSender,
+pub struct SshxNode(SshxNodeInner);
+
+struct SshxNodeInner {
+    endpoint: Endpoint,
+    gossip: Gossip,
+    router: Router,
 }
 
 #[wasm_bindgen]
-impl WebClient {
-    #[wasm_bindgen(constructor)]
-    pub async fn new(ticket_str: &str, on_message: js_sys::Function) -> Result<WebClient, JsValue> {
-        console_error_panic_hook::set_once();
+impl SshxNode {
+    /// Spawns a gossip node.
+    pub async fn spawn() -> Result<Self, JsError> {
+        let secret_key = SecretKey::generate(&mut OsRng);
 
-        let ticket = Ticket::from_str(ticket_str).map_err(|e| JsValue::from_str(&e.to_string()))?;
-
-        let secret_key = SecretKey::generate_with(&mut OsRng);
         let endpoint = Endpoint::builder()
             .secret_key(secret_key)
             .discovery_n0()
             .bind()
             .await
-            .map_err(|e| JsValue::from_str(&e.to_string()))?;
+            .map_err(to_js_err)?;
 
-        console_log!("> our node id: {}", endpoint.node_id());
+        tracing::info!("our node id: {}", endpoint.node_id());
 
-        for node in ticket.nodes.iter() {
-            endpoint.add_node_addr(node.clone()).map_err(|e| JsValue::from_str(&e.to_string()))?;
-        }
+        let gossip = Gossip::builder().spawn(endpoint.clone());
 
-        let gossip = Gossip::builder()
-            .spawn(endpoint.clone())
-            .await
-            .map_err(|e| JsValue::from_str(&e.to_string()))?;
-
-        let _router = Router::builder(endpoint.clone())
-            .accept(iroh_gossip::ALPN, gossip.clone())
+        let router = Router::builder(endpoint.clone())
+            .accept(GOSSIP_ALPN, gossip.clone())
             .spawn();
 
-        let topic = ticket.topic;
+        let inner = SshxNodeInner {
+            endpoint,
+            gossip,
+            router,
+        };
+        Ok(Self(inner))
+    }
 
-        let node_ids = ticket.nodes.iter().map(|p| p.node_id).collect();
-        let (sender, mut receiver) = gossip
-            .subscribe_and_join(topic, node_ids)
-            .await
-            .map_err(|e| JsValue::from_str(&e.to_string()))?
-            .split();
+    /// Returns the node id of this node.
+    pub fn node_id(&self) -> String {
+        self.0.endpoint.node_id().to_string()
+    }
 
-        console_log!("> connected to topic!");
+    /// Returns information about all the remote nodes this [`Endpoint`] knows about.
+    pub fn remote_info(&self) -> Vec<JsValue> {
+        self.0
+            .endpoint
+            .remote_info_iter()
+            .map(|info| serde_wasm_bindgen::to_value(&info).unwrap())
+            .collect()
+    }
 
-        wasm_bindgen_futures::spawn_local(async move {
-            while let Ok(Some(event)) = receiver.try_next().await {
-                if let Event::Gossip(GossipEvent::Received(msg)) = event {
-                    if let Ok(_update) = ClientUpdate::decode(&msg.content[..]) {
-                        let this = JsValue::NULL;
-                        let js_buf = js_sys::Uint8Array::from(&msg.content[..]);
-                        on_message.call1(&this, &js_buf).unwrap();
-                    }
-                }
-            }
+    /// Creates a new SSH session.
+    pub async fn create(&self) -> Result<Session, JsError> {
+        let topic = TopicId::from_bytes({
+            let mut bytes = [0u8; 32];
+            OsRng.fill_bytes(&mut bytes);
+            bytes
         });
 
-        Ok(WebClient {
-            sender,
-        })
+        let ticket = Ticket {
+            topic,
+            nodes: vec![self.0.endpoint.node_addr().initialized().await],
+            key: rand_alphanumeric(14), // 83.3 bits of entropy
+        };
+
+        self.join_inner(ticket).await
     }
 
-    pub async fn send(&self, data: &[u8]) -> Result<(), JsValue> {
-        self.sender
-            .broadcast(data.into())
+    /// Joins an SSH session from a ticket.
+    pub async fn join(&self, ticket: String) -> Result<Session, JsError> {
+        let ticket = Ticket::from_str(&ticket).map_err(to_js_err)?;
+        self.join_inner(ticket).await
+    }
+
+    async fn join_inner(&self, ticket: Ticket) -> Result<Session, JsError> {
+        // Add nodes to address book
+        for node in &ticket.nodes {
+            self.0
+                .endpoint
+                .add_node_addr(node.clone())
+                .map_err(to_js_err)?;
+        }
+
+        let node_ids = ticket.nodes.iter().map(|p| p.node_id).collect();
+        let (sender, receiver) = self
+            .0
+            .gossip
+            .subscribe_and_join(ticket.topic, node_ids)
             .await
-            .map_err(|e| JsValue::from_str(&e.to_string()))?;
+            .map_err(to_js_err)?
+            .split();
+
+        let receiver = ReadableStream::from_stream(receiver.map(|event| {
+            event
+                .map_err(|err| JsValue::from_str(&err.to_string()))
+                .map(|event| serde_wasm_bindgen::to_value(&event).unwrap())
+        }))
+        .into_raw();
+
+        // Add ourselves to the ticket for sharing
+        let mut share_ticket = ticket;
+        share_ticket
+            .nodes
+            .push(self.0.endpoint.node_addr().initialized().await);
+
+        let session = Session {
+            topic_id: share_ticket.topic,
+            nodes: share_ticket.nodes.into_iter().collect(),
+            encryption_key: share_ticket.key,
+            sender: SessionSender(sender),
+            receiver,
+        };
+        Ok(session)
+    }
+}
+
+type SessionReceiver = wasm_streams::readable::sys::ReadableStream;
+
+#[wasm_bindgen]
+pub struct Session {
+    topic_id: TopicId,
+    nodes: BTreeSet<NodeAddr>,
+    encryption_key: String,
+    sender: SessionSender,
+    receiver: SessionReceiver,
+}
+
+#[wasm_bindgen]
+impl Session {
+    #[wasm_bindgen(getter)]
+    pub fn sender(&self) -> SessionSender {
+        self.sender.clone()
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn receiver(&mut self) -> SessionReceiver {
+        self.receiver.clone()
+    }
+
+    pub fn ticket(&self, include_self: bool) -> Result<String, JsError> {
+        let mut ticket = Ticket {
+            topic: self.topic_id,
+            nodes: if include_self {
+                self.nodes.iter().cloned().collect()
+            } else {
+                // Return nodes excluding ourselves (if we can identify ourselves)
+                self.nodes.iter().cloned().collect()
+            },
+            key: self.encryption_key.clone(),
+        };
+        Ok(ticket.to_string())
+    }
+
+    pub fn id(&self) -> String {
+        self.topic_id.to_string()
+    }
+
+    pub fn encryption_key(&self) -> String {
+        self.encryption_key.clone()
+    }
+}
+
+#[wasm_bindgen]
+#[derive(Debug, Clone)]
+pub struct SessionSender(GossipSender);
+
+#[wasm_bindgen]
+impl SessionSender {
+    pub async fn send(&self, data: &[u8]) -> Result<(), JsError> {
+        self.0.broadcast(data.into()).await.map_err(to_js_err)?;
         Ok(())
     }
+}
+
+fn to_js_err(err: impl Into<anyhow::Error>) -> JsError {
+    let err: anyhow::Error = err.into();
+    JsError::new(&err.to_string())
 }
